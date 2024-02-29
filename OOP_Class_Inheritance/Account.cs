dc2eff93aa3c8c30fc20a3e65ac4c81806c7dcea
NahidM8 <nahidbm@code.edu.az>
2024-02-29 18:06:20 +0400

    Add project files.

---
+++ OOP_Class_Inheritance/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class_Inheritance
+{
+    internal class Account
+    {
+        public string username;
+        public string password;
+
+
+        public Account(string Username, string Password)
+        {
+            username=Username; 
+            password=Password;
+        }
+
+    }
+}
---
+++ OOP_Class_Inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP_Class_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        repeat:   
+            Account account = new Account(Console.ReadLine(),Console.ReadLine());
+
+           if (account.username == "Admin" && account.password == "AdminEA18yr@")
+            {
+                Console.WriteLine("giriş ugurludur");
+            }
+           else
+            {
+                Console.WriteLine("username ve ya password sehvdir");
+                goto repeat;
+            }
+
+
+
+
+        }
+    }
+}
